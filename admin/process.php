38f52f6b6fceab60b81ec2d35ee3a5331858417e
TalentChamba <tazchazjnr@gmail.com>
2024-07-01 23:51:18 +0200

    Old - simple way of adding records into the database This is going to be refactored.

--- admin/process.php
+++ admin/process.php
@@ -1,2 +1,15 @@
 <?php
 
+//check if form is submitted
+ if($_SERVER['REQUEST_METHOD'] ==='POST'){
+    include("../connect.php");
+    $title = isset($_POST['title']) ? trim($_POST['title']) : '';
+    $summary = isset($_POST['summary']) ? trim($_POST['summary']) : '';
+    $content = isset($_POST['content']) ? trim($_POST['content']) : '';
+    $date = isset($_POST['date']) ? trim($_POST['date']) :  '';
+
+    $sql ="INSERT INTO posts (title, summary, content, date) VALUES
+    ('$title', '$summary', '$content', '$date')";
+    $result = mysqli_query($conn, $sql);
+
+ }
